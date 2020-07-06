f89fe08409fe7d683f098d1d9f5fd4300a6183e1
Govind B Chandran <govindchandran235@gmail.com>
2020-07-06 11:27:35 +0530

    Create app.js

---
+++ weatherdata/app.js
@@ -0,0 +1,34 @@
+//jshint esversion:6
+
+
+const express = require("express");
+const https = require("https");
+const app = express();
+
+app.get("/",function(req,res){
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=london&appid=258eb97aa8fffc50110cd85b3640dd9e";
+    https.get(url,function (response){
+        console.log(response.statusCode);
+    response.on("data",function (data){
+       const datas = JSON.parse(data);
+       const weatherdata = JSON.stringify(datas);
+       const temp = datas.main.temp;
+       const wdesc = datas.weather[0].description;
+       console.log(weatherdata);
+       console.log(temp);
+       console.log(wdesc);
+    })
+    }) 
+        res.send("Server is up");
+})
+
+
+
+
+
+
+
+
+app.listen(3000, function(){
+    console.log("server is running");
+})
