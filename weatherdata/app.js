27a892d540a3ae05da931ba3c86886545428b9de
Govind B Chandran <govindchandran235@gmail.com>
2020-07-09 00:07:18 +0530

    Update app.js

--- weatherdata/app.js
+++ weatherdata/app.js
@@ -4,9 +4,21 @@
 const express = require("express");
 const https = require("https");
 const app = express();
+const bodyParser = require("body-parser");
+
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/",function(req,res){
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=london&appid=258eb97aa8fffc50110cd85b3640dd9e";
+res.sendFile(__dirname+ "/index.html");
+});
+
+
+app.post("/", function(req, res){
+    const city = req.body.city;
+    const api = "258eb97aa8fffc50110cd85b3640dd9e";
+    const unit = "metric";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=258eb97aa8fffc50110cd85b3640dd9e`;
     https.get(url,function (response){
         console.log(response.statusCode);
     response.on("data",function (data){
@@ -16,9 +28,6 @@ app.get("/",function(req,res){
        const wdesc = datas.weather[0].description;
        const icon = datas.weather[0].icon;
        const imgurl = "http://openweathermap.org/img/wn/"+ icon + "@2x.png";
-       console.log(weatherdata);
-       console.log(temp);
-       console.log(wdesc);
        res.write("<h1>The temperature is "+ temp+ "Kelvin </h1>");
        res.write("<h1>The weather is "+ wdesc);
        res.write("<img src=" + imgurl + ">");
@@ -26,12 +35,6 @@ app.get("/",function(req,res){
     })
     }) 
 })
-
-
-
-
-
-
 
 
 app.listen(3000, function(){
