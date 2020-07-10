ae8ea46333bf0b0a2ea89709a1db8043c6751561
Govind B Chandran <govindchandran235@gmail.com>
2020-07-10 11:19:52 +0530

    Update app.js

--- mailchimp/app.js
+++ mailchimp/app.js
@@ -5,22 +5,20 @@ const bodyParser = require("body-parser");
 const request = require("request");
 const app = express();
 
-app.get("/", function(req,res){
-    res.send("Working");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+app.post("/",function (req, res){
+    var first = req.body.first;
+    var last = req.body.last;
+    var email = req.body.mail;
+    console.log(first,last,email);
+
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get("/", function(req,res){
+    res.sendFile(__dirname+"/signup.html");
+})
 
 
 
