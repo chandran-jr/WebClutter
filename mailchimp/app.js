44782641cc78776386601076a2c5484f7e285151
Govind B Chandran <govindchandran235@gmail.com>
2020-07-13 18:12:05 +0530

    Mailchimp and Heroku

--- mailchimp/app.js
+++ mailchimp/app.js
@@ -1,6 +1,6 @@
 //jshint esversion:6
 
-const express = require("express")
+const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 const app = express();
