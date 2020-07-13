6e7ae1a0ed1262f682e94174cba8df43cd2cd5ac
Govind B Chandran <govindchandran235@gmail.com>
2020-07-13 18:12:46 +0530

    Hosted a web app

--- mailchimp/app.js
+++ mailchimp/app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 const app = express();
-const https = require("https")
+const https = require("https");
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
