61fa2eeac66027952e4c30bd7f9548d53fae9dbe
Govind B Chandran <govindchandran235@gmail.com>
2020-07-15 21:48:31 +0530

    Update app.js

--- todolist/app.js
+++ todolist/app.js
@@ -10,25 +10,14 @@ app.set('view engine', 'ejs');
 
 app.get("/", function (req, res){
     var today = new Date();
-    var currentday = today.getDay();
-    var day = "";
-switch (currentday){
-    case 0: day = "Sunday";
-    break;
-    case 1: day = "Monday";
-    break;
-    case 2: day = "Tuesday";
-    break;
-    case 3: day = "Wednesday";
-    break;
-    case 4: day = "Thursday";
-    break;
-    case 5: day = "Friday";
-    break;
-    case 6: day = "Saturday";
-    break;
-    default: console.log("Error");
-}
+    
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    var day = today.toLocaleDateString("en-US", options);
 
         res.render("index",{kindofday: day});
 })
