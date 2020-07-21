ae45a710f4f1b5b183fdacd44846b6037e84ceef
Govind B Chandran <govindchandran235@gmail.com>
2020-07-22 00:43:07 +0530

    Update app.js

--- todolist/app.js
+++ todolist/app.js
@@ -48,6 +48,10 @@ app.get("/", function (req, res){
         res.render("index",{listtitle: day, newlistitems: items});
 })
 
+app.get("/about",function(req,res){
+    res.render("about");
+})
+
 app.listen("3000", function(){
     console.log("Server running");
 });
